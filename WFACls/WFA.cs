78e357a144c70a0e6731bc806b6d71d841f385fe
pdhakad <pdhakad@AZ-002>
2022-02-01 09:20:17 +0530

    Add project files.

---
+++ WFACls/WFA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFACls
+{
+    public class WFA
+    {
+        public float Add(float a,float b)
+        {
+            return a + b;
+        }
+        public float Sub(float a, float b)
+        {
+            return a - b;
+        }
+        public float Mul(float a, float b)
+        {
+            return a * b;
+        }
+        public float Div(float a, float b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ WFADemo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFACls;
+namespace WFADemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WFA w = new WFA();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float x=Convert.ToInt32(textBox1.Text);
+            float y=Convert.ToInt32(textBox2.Text);
+            textBox3.Text=Convert.ToString(w.Add(x,y));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float x=Convert.ToInt32(textBox1.Text);
+            float y=Convert.ToInt32(textBox2.Text);
+            textBox3.Text=Convert.ToString(w.Sub(x,y));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float x = Convert.ToInt32(textBox1.Text);
+            float y=Convert.ToInt32(textBox2.Text);
+            textBox3.Text=Convert.ToString((w.Mul(x,y)));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float x=Convert.ToInt32((textBox1.Text));
+            float y=Convert.ToInt32((textBox2.Text));
+            textBox3.Text = Convert.ToString((w.Div(x, y)));
+        }
+    }
+}
